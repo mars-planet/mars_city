4432d0151b7575271449abec61cab27b40177931
Ezio Melotti <ezio.melotti@gmail.com>
2013-02-10 13:26:16 +0100

    Add a minimal library to get data from Tango devices.

---
+++ server.py
@@ -0,0 +1,16 @@
+import sys
+import json
+import PyTango
+
+def get_data(device):
+    dev = PyTango.DeviceProxy(device)
+    return dict((attr, dev[attr].value) for attr in dev.get_attribute_list())
+
+def get_json_data(device):
+    return json.dumps(get_data(device))
+
+if __name__ == '__main__':
+    dev = sys.argv[1]
+
+    print 'data:', get_data(dev)
+    print 'json:', get_json_data(dev)
