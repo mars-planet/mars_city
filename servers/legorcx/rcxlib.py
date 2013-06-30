0e2b41c5600fdc041f28bed663815f6a5d64b855
Ezio Melotti <ezio.melotti@gmail.com>
2013-06-30 13:40:48 +0200

    Add rcxlib.py and a few basic commands in legorcx.

---
+++ servers/legorcx/rcxlib.py
@@ -0,0 +1,94 @@
+"""Module to send commands via IR to a LEGO RCX 2.0"""
+
+# Written by Ezio Melotti
+# Based on the rcx.cpp library by Daniel Berger
+
+# Sending two commands in rapid succession doesn't work,
+# so we need to sleep at least 0.1s between them.
+# The code should be optimized to group similar commands
+# (e.g. setting the same speed on two motors).
+from time import sleep
+
+# To create /dev/usb/lego0, do the following:
+#   sudo modprobe legousbtower
+#   sudo ln -s /dev/usb/legousbtower0 /dev/usb/lego0
+#   sudo chmod 777 /dev/usb/lego0
+# On Windows try to use "\\\\.\\LEGOTOWER1" (untested)
+
+class RCX(object):
+    def __init__(self, path='/dev/usb/lego0'):
+        self.ir = open('/dev/usb/lego0', 'wb', 0)
+        self.switchbit = 0
+        self.motors_on = [0, 0, 0]
+        self.motors_speeds = [0, 0, 0]
+        self.motors_directions = [1, 1, 1]
+
+    def set_motors(self, A, B, C):
+        motors = [A, B, C]
+        if motors == self.motors_on:
+            return
+        byte = A*1 + B*2 + C*4
+        if not all(motors):
+            # turn all motors
+            self._send('\x21\x47')
+            sleep(0.1)
+        if any(motors):
+            # turn on motors
+            msg = '\x21' + chr(byte+0x80)
+            self._send(msg)
+        self.motors_on = motors
+
+    def set_speeds(self, A, B, C):
+        speeds = [A, B, C]
+        if speeds == self.motors_speeds:
+            return
+        if A != self.motors_speeds[0]:
+            self._send('\x13\x01\x02' + chr(abs(A)-1))
+            sleep(0.1)
+        if B != self.motors_speeds[1]:
+            self._send('\x13\x02\x02' + chr(abs(B)-1))
+            sleep(0.1)
+        if C != self.motors_speeds[2]:
+            self._send('\x13\x04\x02' + chr(abs(C)-1))
+        self.motors_speeds = speeds
+
+    def set_directions(self, A, B, C):
+        directions = [A, B, C]
+        if directions == self.motors_directions:
+            return
+        if A:
+            self._send('\xe1\x81' if A > 0 else '\xe1\x01')
+            sleep(0.1)
+        if B:
+            self._send('\xe1\x82' if B > 0 else '\xe1\x02')
+            sleep(0.1)
+        if C:
+            self._send('\xe1\x84' if C > 0 else '\xe1\x04')
+        self.motors_directions = directions
+
+    def _send(self, msg):
+        print '>>>', [hex(ord(x)) for x in msg]
+        # maximal length of msg is 125 bytes
+        s = 0
+        #create message to send
+        sendbuf = '\x55\xff\x00'
+        fst = ord(msg[0]) | self.switchbit
+        self.switchbit ^= 0x08
+        msg = chr(fst) + msg[1:]
+        for c in msg:
+            sendbuf += c
+            sendbuf += chr(ord(c)^0xff)
+            s += ord(c)
+        s = s % 256 # cast to unsigned char
+        sendbuf += chr(s)
+        sendbuf += chr(s^0xff)
+        print [hex(ord(x)) for x in sendbuf]
+        return self.ir.write(sendbuf)
+
+    def beep(self, sound=1):
+        # 0: blip, 1: beep beep, 2: downward tones,
+        # 3: upward tones, 4: low buzz, 5: fast upward tones
+        if sound < 0 or sound > 5:
+            raise ValueError('sound should be in range(6)')
+        msg = '\x51' + chr(sound)
+        self._send(msg)
