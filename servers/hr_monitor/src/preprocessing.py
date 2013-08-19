5d00d0ba8b282f175f872996395f2a7ab1de9746
mtambos <mtambos@gmail.com>
2013-08-19 17:48:05 +0200

    add acceleration components (x, y and z vectors, besides magnitude) to output data frame.
    Former-commit-id: 0376ea7532c2819969b8d1adda05b68fc4563cdc

--- servers/hr_monitor/src/preprocessing.py
+++ servers/hr_monitor/src/preprocessing.py
@@ -1,9 +1,10 @@
-from pylab import sqrt
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
 
 def magnitude(row):
-    return sqrt(row[21]**2 + row[22]**2 + row[23]**2)
+    return (row[21]**2 + row[22]**2 + row[23]**2)**.5
 
 
 def read_data(path):
@@ -22,14 +23,17 @@ def read_data(path):
 
     df = pd.read_csv(path, sep='\s*', names=colum_names, header=None)
     df['IMU_Chest_Magnitude'] = df.apply(magnitude, axis=1)
-    df = df.loc[:, ['timestamp', 'activityID', 'hr', 'IMU_Chest_Magnitude']]
+    df['IMU_Chest_x'] = df['IMU_Chest1']
+    df['IMU_Chest_y'] = df['IMU_Chest2']
+    df['IMU_Chest_z'] = df['IMU_Chest3']
+    df = df.loc[:, ['timestamp', 'activityID', 'hr', 'IMU_Chest_Magnitude',
+                    'IMU_Chest_x', 'IMU_Chest_y', 'IMU_Chest_z']]
 
-    timestamps = df['timestamp']
-    df['timestamp'] = timestamps.apply(lambda x: x*10**9)
-    df['timestamp'] = timestamps.astype('datetime64[ns]')
+    now = datetime.now()
+    df['timestamp'] += (now - datetime(1970,1,1)).total_seconds()
+    df['timestamp'] = df['timestamp'].apply(lambda x: x*10**9)
+    df['timestamp'] = df['timestamp'].astype('datetime64[ns]')
     df = df.set_index('timestamp')
-
-    df['activityID'] = df['activityID'].astype('string')
 
     return df
 
@@ -44,15 +48,18 @@ def extract_hr_acc(dataframe):
             ratio -> hr/acc
             ratio_log -> ln(ratio)
     """
-    df = pd.DataFrame({'hr': dataframe.hr.tolist(),
-                       'acc': dataframe.IMU_Chest_Magnitude.tolist()},
+    df = pd.DataFrame({'hr': dataframe.hr,
+                       'acc': dataframe.IMU_Chest_Magnitude,
+                       'acc_x': dataframe.IMU_Chest_x,
+                       'acc_y': dataframe.IMU_Chest_y,
+                       'acc_z': dataframe.IMU_Chest_z},
                         index=dataframe.index)
+    df['ratio'] = df.hr/df.acc
+    df['ratio_log'] = np.log(df.ratio)
     #fill NAs forward
     df = df.fillna(method='ffill')
     #fill NAs backwards (fill any NAs at the start of all series)
     df = df.fillna(method='bfill')
-    df['ratio'] = df.hr/df.acc
-    df['ratio_log'] = np.log(df.ratio)
 
     #df = df.reset_index()
     return df
