336a15c9becf5b54a0292dab49f7e1eeb5a14933
mtambos <mtambos@gmail.com>
2013-09-02 11:32:52 +0200

    Add milliseconds column (float) to Datapoint's and Alarm's key (to be able to insert records with millisecond/microsecond accuracy).
    Former-commit-id: 0b15b80de95cfb25e0358394a3d2c75962ed8458

--- servers/hr_monitor/src/data_model.py
+++ servers/hr_monitor/src/data_model.py
@@ -1,33 +1,17 @@
+from __future__ import division
+
+from datetime import datetime, timedelta
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, Float
-from datetime import datetime
 
 Base = declarative_base()
-
-
-class Alarm(Base):
-    __tablename__ = 'alarms'
-    timestamp = Column(DateTime, primary_key=True)
-    alarm_lvl = Column(Float, nullable=False)
-    sgmt_begin = Column(DateTime, nullable=False)
-    sgmt_end = Column(DateTime, nullable=False)
-    doe = Column(DateTime, default=datetime.now)
-
-    def __init__(self, timestamp, alarm_lvl, sgmt_begin, sgmt_end):
-        self.timestamp = timestamp
-        self.alarm_lvl = alarm_lvl
-        self.sgmt_begin = sgmt_begin
-        self.sgmt_end = sgmt_end
-
-    def __repr__(self):
-        return ("<Alarm('%s','%s', '%s', '%s')>"
-                % (self.timestamp, self.alarm_lvl,
-                   self.sgmt_begin, self.sgmt_end))
 
 
 class Datapoint(Base):
     __tablename__ = 'datapoints'
     timestamp = Column(DateTime, primary_key=True)
+    millisecond = Column(Float, primary_key=True)
     hr = Column(Float)
     acc_x = Column(Float)
     acc_y = Column(Float)
@@ -35,8 +19,13 @@ class Datapoint(Base):
     acc_magn = Column(Float)
     doe = Column(DateTime, default=datetime.now)
 
-    def __init__(self, timestamp, hr, acc_x, acc_y, acc_z):
-        self.timestamp = timestamp
+    def __init__(self, timestamp, hr, acc_x, acc_y, acc_z, millisecond=None):
+        if millisecond:
+            self.timestamp = timestamp
+            self.timestamp += timedelta(microseconds=millisecond * 1000)
+        else:
+            self.timestamp = timestamp
+            self.millisecond = timestamp.microsecond / 1000
         self.hr = hr
         self.acc_x = acc_x
         self.acc_y = acc_y
@@ -47,3 +36,35 @@ class Datapoint(Base):
         return ("<Datapoint('%s','%s','%s','%s','%s','%s')>"
                 % (self.timestamp, self.hr,
                    self.acc_x, self.acc_y, self.acc_z, self.acc_magn))
+
+
+class Alarm(Base):
+    __tablename__ = 'alarms'
+    timestamp = Column(DateTime, primary_key=True)
+    millisecond = Column(Float, primary_key=True)
+    alarm_lvl = Column(Float, nullable=False)
+    sgmt_begin = Column(DateTime, nullable=False)
+    sgmt_end = Column(DateTime, nullable=False)
+
+    def __init__(self, alarm_lvl, sgmt_begin, sgmt_end,
+                 timestamp=None, millisecond=None):
+        if not timestamp:
+            self.timestamp = datetime.now()
+            self.millisecond = self.timestamp.microsecond / 1000
+        else:
+            self.timestamp = timestamp
+        if timestamp and millisecond:
+            self.timestamp = timestamp
+            self.timestamp += timedelta(microseconds=millisecond * 1000)
+            self.millisecond = millisecond
+        if not millisecond:
+            self.millisecond = self.timestamp.microsecond / 1000
+
+        self.alarm_lvl = alarm_lvl
+        self.sgmt_begin = sgmt_begin
+        self.sgmt_end = sgmt_end
+
+    def __repr__(self):
+        return ("<Alarm('%s','%s', '%s', '%s')>"
+                % (self.timestamp, self.alarm_lvl,
+                   self.sgmt_begin, self.sgmt_end))
