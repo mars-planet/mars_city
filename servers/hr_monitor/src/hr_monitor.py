4cca0d4bff76967a81b86f0b832b23c83842e437
mtambos <mtambos@gmail.com>
2013-09-02 11:39:14 +0200

    Move from SQLite to MySQL and perform anomaly detection in separate thread.
    Former-commit-id: 7691f7b4c046b5181569cb379d6cdaa82fcd3b9d

--- servers/hr_monitor/src/hr_monitor.py
+++ servers/hr_monitor/src/hr_monitor.py
@@ -2,12 +2,15 @@
 
 from __future__ import division, print_function
 from datetime import datetime, timedelta
+from collections import deque
+
+from threading import Thread
 
 from numpy import mean, nan
 from pandas import DataFrame
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.exc import IntegrityError
 
 from data_model import Datapoint, Alarm, Base
 from assumption_free import AssumptionFreeAA as Detector
@@ -15,94 +18,119 @@ from assumption_free import AssumptionFreeAA as Detector
 
 class HRMonitor(object):
 
-    def __init__(self, engine=None, conn_str='sqlite:///hr_monitor.db',
+    # for sqlite use conn_str='sqlite:///hr_monitor.db'
+    def __init__(self, engine=None,
+                 conn_str='mysql+mysqldb://root@localhost/hr_monitor',
                  commit_on_request=True):
         print('Constructing HRMonitor')
         if engine is not None:
             self.engine = engine
         else:
             print('Setting up SQLite DB')
-            self.engine = create_engine(conn_str)
-            conn = self.engine.connect()
-            if not self.engine.dialect.has_table(conn, "alarms"):
+            engn = create_engine(conn_str)
+            conn = engn.connect()
+            if not engn.dialect.has_table(conn, "alarms"):
                 print('Creating Tables')
-                Base.metadata.create_all(self.engine)
+                Base.metadata.create_all(engn)
+            conn.close()
+            engn.dispose()
+            self.engine = create_engine(conn_str)
         self.Session = sessionmaker(bind=self.engine)
         self._current_session = None
         self._commit_on_request = commit_on_request
-        self.resolution = 2000  # in millisecs
+        self.resolution = 1000  # in millisecs
+        self.last_alarm_timestamp = datetime.now()
         print('Constructing Detector')
-        self.detector = Detector(word_size=10, window_factor=3,
-                                 lead_window_factor=3, lag_window_factor=12,
+        self.detector = Detector(word_size=5, window_factor=2,
+                                 lead_window_factor=2, lag_window_factor=4,
                                  recursion_level=2)
         print('Finished constructing HRMonitor')
+        self.timestamps = deque(maxlen=self.detector.universe_size)
 
     def __del__(self):
         print('Cleaning up HRMonitor')
         self._get_session().commit()
         self.Session.close_all()
         del self.Session
+        self._sqlconn.close()
+        del self._sqlconn
         self.engine.dispose()
         del self.engine
 
-    def _get_session(self):
-        if not self._current_session:
-            self._current_session = self.Session()
-        return self._current_session
-
-    def _session_commit(self):
-        if self._commit_on_request:
-            self._current_session.commit()
-            self._current_session = None
-
-    def _session_close(self):
-        if self._commit_on_request:
-            self._current_session.close()
-            self._current_session = None
-
     def _get_avgs(self, period):
         session = self._get_session()
-        query = session.query(Datapoint)
-        query = query.filter(Datapoint.timestamp
-                            > datetime.now() - timedelta(seconds=period)
-                            )
-        data = [[x.hr, x.acc_magn] for x in query.all()]
-        if len(data) > 0:
-            avgs = mean(data, axis=0)
-        else:
-            avgs = [nan, nan]
-        self._session_close()
+        try:
+            query = session.query(Datapoint)
+            query = query.filter(Datapoint.timestamp
+                                > datetime.now() - timedelta(seconds=period)
+                                )
+            data = [[x.hr, x.acc_magn] for x in query.all()]
+            if len(data) > 0:
+                avgs = mean(data, axis=0)
+            else:
+                avgs = [nan, nan]
+        finally:
+            session.close()
         return {'hr': avgs[0], 'acc': avgs[1]}
 
-    def register_datapoint(self, args):
-        dp = Datapoint(*args)
-        session = self._get_session()
-        session.add(dp)
-        self._session_commit()
-        query = session.query(Datapoint)
-        query = query.filter(Datapoint.timestamp
-                             > self.detector.last_timestamp)
-        datapoints = query.all()
-        self._session_close()
-        timerange = (dp.timestamp - datapoints[0].timestamp).total_seconds()
-        timerange *= 1000
-        if datapoints and  timerange >= self.resolution:
-            datapoints.append(dp)
-            first_dp = datapoints[0]
-            datapoints = DataFrame({
-                            'timestamp': [d.timestamp for d in datapoints],
-                            'ratio': [d.hr / d.acc_magn for d in datapoints]
-                        })
-            datapoints.set_index('timestamp', inplace=True)
-            datapoints = datapoints.resample('%sms' % (self.resolution))
-            analysis = self.detector.detect(datapoints.ratio, dp.timestamp)
-            if analysis:
-                analysis = analysis[0]
-                session = self._get_session()
-                session.add(Alarm(dp.timestamp, analysis.score,
-                                  first_dp.timestamp, dp.timestamp))
-                self._session_commit()
-                self._session_close()
+    def register_datapoint(self, *args):
+        timestamp, hr, acc_x, acc_y, acc_z = args
+        dp = Datapoint(timestamp=timestamp, hr=hr,
+                       acc_x=acc_x, acc_y=acc_y, acc_z=acc_z)
+        session = self.Session()
+        try:
+            session.add(dp)
+            session.commit()
+            t = Thread(target=self._generate_alarms)
+            t.start()
+        finally:
+            session.close()
+
+    def _generate_alarms(self):
+        timerange = datetime.now() - self.last_alarm_timestamp
+        timerange = timerange.total_seconds() * 1000
+        if timerange >= self.resolution:
+            self.last_alarm_timestamp = datetime.now()
+            session = self.Session()
+            try:
+                query = session.query(Datapoint)
+                query = query.filter(Datapoint.timestamp
+                                     > self.detector.last_timestamp)
+                datapoints = query.all()
+                datapoints = DataFrame(
+                       {
+                        'timestamp': [d.timestamp for d in datapoints],
+                        'ratio': [d.hr / d.acc_magn for d in datapoints]
+                       })
+                datapoints.set_index('timestamp', inplace=True)
+                datapoints = datapoints.resample('%sms' % self.resolution)
+                datapoints = datapoints.fillna(method='ffill')
+                if len(datapoints) > 0:
+                    last_timestamp = datapoints.index.max()
+                    self.timestamps.extend(datapoints.index.tolist())
+                    analysis = self.detector.detect(datapoints.ratio,
+                                                    last_timestamp)
+                    if analysis:
+                        first_timestamp = self.timestamps.popleft()
+                        analysis = analysis[0]
+                        session.add(
+                                Alarm(timestamp=last_timestamp,
+                                      alarm_lvl=analysis.score,
+                                      sgmt_begin=first_timestamp,
+                                      sgmt_end=last_timestamp))
+                        try:
+                            session.commit()
+                        except IntegrityError:
+                            session.rollback()
+                            session.add(
+                                    Alarm(timestamp=last_timestamp
+                                            + timedelta(microseconds=100),
+                                          alarm_lvl=analysis.score,
+                                          sgmt_begin=first_timestamp,
+                                          sgmt_end=last_timestamp))
+                            session.commit()
+            finally:
+                session.close()
 
     def get_avg_hr(self, period):
         return self._get_avgs(period)['hr']
@@ -111,13 +139,15 @@ class HRMonitor(object):
         return self._get_avgs(period)['acc']
 
     def get_current_alarms(self, period):
-        session = self._get_session()
-        query = session.query(Alarm)
-        query = query.filter(Alarm.timestamp
-                            > datetime.now() - timedelta(seconds=period)
-                            )
-        results = query.all()
-        self._session_close()
+        session = self.Session()
+        try:
+            query = session.query(Alarm)
+            query = query.filter(Alarm.timestamp
+                                > datetime.now() - timedelta(seconds=period)
+                                )
+            results = query.all()
+        finally:
+            session.close()
         return results
 
 
