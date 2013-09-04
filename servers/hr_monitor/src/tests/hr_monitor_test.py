5b6d1a11af876ecf35376574e5571fbe2b0b0fc6
mtambos <mtambos@gmail.com>
2013-09-04 14:16:57 +0200

    Downsample data.
    Former-commit-id: da40914eb35f8d5b84bfcc87f6ba00d591d81ad8

--- servers/hr_monitor/src/tests/hr_monitor_test.py
+++ servers/hr_monitor/src/tests/hr_monitor_test.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
+from __future__ import division, print_function
 
 import os
 import sys
 from collections import namedtuple
-from datetime import datetime
 from time import sleep
+from datetime import datetime
 
 from PyTango import DeviceProxy
 
@@ -22,20 +22,20 @@ if __name__ == '__main__':
     filename = os.path.join(dirname, 'dataset.dat')
     print(filename)
     data = extract_hr_acc(read_data(filename))
+    data = data.resample('1000L')
 
     DP = namedtuple("DP", ["timestamp", "hr", "acc_x", "acc_y", "acc_z"])
     i = 0
-    percentiles = int(len(data) / 1000)
     prow, ptimestamp = None, None
     for index, row in data.iterrows():
         timestamp = float(index.to_datetime().strftime('%s.%f'))
-        if i % percentiles == 0:
-            print("%s%%" % (i / percentiles))
+        progress = (i / len(data)) * 100
         if prow is not None:
             datapoint = DP(timestamp=ptimestamp, hr=prow['hr'],
                            acc_x=prow['acc_x'], acc_y=prow['acc_y'],
                            acc_z=prow['acc_z'])
-            print("%s, %s" % (index.to_datetime(), datapoint.hr))
+            print("%s - %s, %s - %s%%" % (datetime.now(), index.to_datetime(),
+                                          datapoint.hr, progress))
             proxy.register_datapoint(datapoint)
             sleep(timestamp - ptimestamp)
         prow, ptimestamp = row, timestamp
