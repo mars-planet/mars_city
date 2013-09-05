6a9657fee30872d3c012d14498186b8be914a7e3
Simar <skoder91@gmail.com>
2013-09-05 16:52:44 +0530

    Shift the traindata output, to make it relevant for prediction training
    Former-commit-id: f1c6581870538f231db2137b9ee11543d2bca36d

--- servers/solarstorm/neuralnetwork/parser.py
+++ servers/solarstorm/neuralnetwork/parser.py
@@ -15,44 +15,58 @@ class Parser(object):
         out_csv = csv.writer(open(csv_file, 'wb'))
         
         for year in range(2001, 2013):
-            with open("{}/dsd_{}.txt".format(self.src, year), 'r') as in_txt:
+            with open("{0}/dsd_{1}.txt".format(self.src, year), 'r') as in_txt:
                 if year == 2008:
                     # The data in year 2008 seems to quite inconsistent
                     continue
-                    
+                
+                self.index = 0    
                 for line in in_txt:
                     row_data = []
                     if line.startswith((':', '#')):
                         continue
                     values = line.split()
                     
-                    date = '-'.join(values[:3])
-                    radioflux = values[3]
-                    sunspotnum = values[4]
-                    sunspotarea = values[5]
-                    newregs = values[6]
-                    row_data.extend((date, radioflux, sunspotnum,
-                                     sunspotarea, newregs))
-                    
-                    # Currently assigning 0 to the missing values (*, Unk),
-                    # but can be changed later to a better substitute value
-                    bkgdflux_alpha = self._bkgdflux(values[8][0])
-                    if (values[8] not in ("*","Unk")) :
-                        bkgdflux_float = float(values[8][1:])
-                    else :
-                        bkgdflux_float = 0.0
+                    if self.index == 0:
+                        print "yeah"
+                        self.pdate = '-'.join(values[:3])
+                        self.pradioflux = values[3]
+                        self.psunspotnum = values[4]
+                        self.psunspotarea = values[5]
+                        self.pnewregs = values[6]                        
                         
-                    row_data.extend((bkgdflux_alpha, bkgdflux_float))
-                    
-                    # Using the C,M,X classification and not using the
-                    # optical S,1,2,3 classification as it is no longer
-                    # widely used
-                    cflare = values[9]
-                    mflare = values[10]
-                    xflare = values[11]
-                    row_data.extend((cflare, mflare, xflare))
-                    
-                    out_csv.writerow(row_data)
+                        self.pbkgdflux_alpha = self._bkgdflux(values[8][0])
+                        if (values[8] not in ("*","Unk")) :
+                            self.pbkgdflux_float = float(values[8][1:])
+                        else :
+                            self.pbkgdflux_float = 0.0
+                        
+                    else :
+                        print "asdf"
+                        row_data.extend((self.pdate, self.pradioflux, self.psunspotnum,
+                                         self.psunspotarea, self.pnewregs, self.pbkgdflux_alpha, 
+                                         self.pbkgdflux_float))
+                        
+                        cflare = values[9]
+                        mflare = values[10]
+                        xflare = values[11]
+                        row_data.extend((cflare, mflare, xflare))
+                                                                        
+                        out_csv.writerow(row_data)
+                                                
+                        self.pdate = '-'.join(values[:3])
+                        self.pradioflux = values[3]
+                        self.psunspotnum = values[4]
+                        self.psunspotarea = values[5]
+                        self.pnewregs = values[6]
+                        self.pbkgdflux_alpha = self._bkgdflux(values[8][0])
+                        if (values[8] not in ("*","Unk")) :
+                            self.pbkgdflux_float = float(values[8][1:])
+                        else :
+                            self.pbkgdflux_float = 0.0
+                        
+                    self.index += 1
+                    print self.index        
                     
     def _bkgdflux(self, bkgd):
         # The bkgd flux is in such string format like B1.2, A3.0, C3.6
