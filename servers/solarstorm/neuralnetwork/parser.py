091aa5cd97ca8d44890eb37ecc0f0c2bf5852192
Simar <skoder91@gmail.com>
2013-09-05 18:59:45 +0530

    Some corrections and Removed debugging statements
    Former-commit-id: 7d0e2f4b7da3bc9a249bc1db27c14d0c4d68efb6

--- servers/solarstorm/neuralnetwork/parser.py
+++ servers/solarstorm/neuralnetwork/parser.py
@@ -13,47 +13,24 @@ class Parser(object):
     def writecsv(self):
         csv_file = "traindata.csv"
         out_csv = csv.writer(open(csv_file, 'wb'))
-        
+
         for year in range(2001, 2013):
             with open("{0}/dsd_{1}.txt".format(self.src, year), 'r') as in_txt:
                 if year == 2008:
                     # The data in year 2008 seems to quite inconsistent
                     continue
-                
-                self.index = 0    
+
+                self.index = 0
                 for line in in_txt:
                     row_data = []
                     if line.startswith((':', '#')):
                         continue
                     values = line.split()
-                    
+
                     if self.index == 0:
-                        print "yeah"
-                        self.pdate = '-'.join(values[:3])
-                        self.pradioflux = values[3]
-                        self.psunspotnum = values[4]
-                        self.psunspotarea = values[5]
-                        self.pnewregs = values[6]                        
-                        
-                        self.pbkgdflux_alpha = self._bkgdflux(values[8][0])
-                        if (values[8] not in ("*","Unk")) :
-                            self.pbkgdflux_float = float(values[8][1:])
-                        else :
-                            self.pbkgdflux_float = 0.0
-                        
-                    else :
-                        print "asdf"
-                        row_data.extend((self.pdate, self.pradioflux, self.psunspotnum,
-                                         self.psunspotarea, self.pnewregs, self.pbkgdflux_alpha, 
-                                         self.pbkgdflux_float))
-                        
-                        cflare = values[9]
-                        mflare = values[10]
-                        xflare = values[11]
-                        row_data.extend((cflare, mflare, xflare))
-                                                                        
-                        out_csv.writerow(row_data)
-                                                
+                        # For the first instance, using only the inputs.
+                        # All values preceded by 'p' are meant to refer
+                        # the previous day values i.e. inputs
                         self.pdate = '-'.join(values[:3])
                         self.pradioflux = values[3]
                         self.psunspotnum = values[4]
@@ -64,14 +41,39 @@ class Parser(object):
                             self.pbkgdflux_float = float(values[8][1:])
                         else :
                             self.pbkgdflux_float = 0.0
-                        
+
+                    else :
+                        row_data.extend((self.pdate, self.pradioflux,
+                                         self.psunspotnum, self.psunspotarea,
+                                         self.pnewregs, self.pbkgdflux_alpha,
+                                         self.pbkgdflux_float))
+
+                        cflare = values[9]
+                        mflare = values[10]
+                        xflare = values[11]
+                        row_data.extend((cflare, mflare, xflare))
+
+                        # Write the csv data with shifted output
+                        out_csv.writerow(row_data)
+
+                        # Updating the 'p' values to current values
+                        self.pdate = '-'.join(values[:3])
+                        self.pradioflux = values[3]
+                        self.psunspotnum = values[4]
+                        self.psunspotarea = values[5]
+                        self.pnewregs = values[6]
+                        self.pbkgdflux_alpha = self._bkgdflux(values[8][0])
+                        if (values[8] not in ("*","Unk")) :
+                            self.pbkgdflux_float = float(values[8][1:])
+                        else :
+                            self.pbkgdflux_float = 0.0
+
                     self.index += 1
-                    print self.index        
-                    
+
     def _bkgdflux(self, bkgd):
         # The bkgd flux is in such string format like B1.2, A3.0, C3.6
         # So, to handle these alphanumeric values, an integer value
-        # is being associated with the alphabetic part. 
+        # is being associated with the alphabetic part.
         # Please note, this is an experimental method to handle these
         # values, and may be changed if a better solution is found
         flux = dict(A=1, B=2, C=3, M=4)
