7571fdc906a5f47e5468f68349f83e56a0ae17db
Ezio Melotti <ezio.melotti@gmail.com>
2013-09-11 01:11:30 +0300

    Remove commented-out code and clean up whitespace.
    Former-commit-id: a1ae91bb70d4b215895bb1a41fc971a533cb9952

--- servers/neurohs/neurohs.py
+++ servers/neurohs/neurohs.py
@@ -3,9 +3,8 @@ from __future__ import division
 import sys
 from collections import deque
 
-
-from emokit import emotiv
 import gevent
+from emokit import emotiv
 
 if __name__ == "__main__":
     headset = emotiv.Emotiv()
@@ -27,11 +26,6 @@ if __name__ == "__main__":
     try:
         while True:
             packet = headset.dequeue()
-            #print 'size', headset.packets.qsize()
-            #print x-packet.gyroX, y-packet.gyroY,
-            #print packet.gyroX, packet.gyroY
-            #turtle.up()
-            #xpos, ypos = turtle.pos()
             xdiff = x-packet.gyroX
             ydiff = y-packet.gyroY
             if abs(xdiff) > 0:
@@ -41,25 +35,14 @@ if __name__ == "__main__":
             if abs(ydiff) > 0:
                 yspeed -= ydiff
                 ypos += int(round(yspeed))
-            #ypos = max(min(ypos, ymax), 0)
-            #print '%4d %4d' % (xspeed, yspeed)
-            #print '%4d %4d' % (xpos, ypos)
-            #move_pointer(xpos, ypos)
-            #turtle.goto(xpos, ypos)
-            #turtle.down()
             x, y = packet.gyroX, packet.gyroY
             xs.append(x)
             ys.append(y)
-            #print len(xs)
             if len(xs) >= size:
                 print '%d;%d' %(sum(xs)/size,  sum(ys)/size)
-                #sys.stderr.write('e %d;%d\n' %(sum(xs)/size,  sum(ys)/size))
                 sys.stdout.flush()
                 xs.clear()
                 ys.clear()
-            #print 'x y %4.2f % 4.2f\r' %(sum(xs)/size,  sum(ys)/size),
-            #gevent.sleep(.01)
-            #headset.packets.empty()
     except KeyboardInterrupt:
         headset.close()
     finally:
