d2759104149466ddb5c010870c8542c877f7bb05
Ezio Melotti <ezio.melotti@gmail.com>
2013-09-11 02:47:36 +0300

    Expose all the headset data to the tango server.
    Former-commit-id: e574efbf72d27be7897aa64ad64d17012160d28f

--- servers/neurohs/neurohs.py
+++ servers/neurohs/neurohs.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import sys
+import json
 from collections import deque
 
 import gevent
@@ -22,25 +23,44 @@ if __name__ == "__main__":
     # in a queue 128Hz / 5Hz = ~25 packets and write on stdout the
     # average values of the 25 packets before resetting the queue.
     size = int(128 / (1/polling))
+    packets = deque(maxlen=size)
+
     print '-----'
     sys.stderr.write('START\n')
 
-    # don't move the headset while reading this
-    packet = headset.dequeue()
-    x, y = packet.gyroX, packet.gyroY
-    xs, ys = deque(maxlen=size), deque(maxlen=size)
-
+    # Read the packets in loop and add them to the queue.
+    # Once enough packets are collected, calculate the average of the values,
+    # serialize the result with json, print it on stdout, and reset the queue
     try:
         while True:
             packet = headset.dequeue()
-            x, y = packet.gyroX, packet.gyroY
-            xs.append(x)
-            ys.append(y)
-            if len(xs) >= size:
-                print '%d;%d' %(sum(xs)/size,  sum(ys)/size)
+            # We only need the sensors data + the battery.
+            # The rawdata, counter, and sync are not needed;
+            # the other values are redundant
+            data = dict(packet.sensors)
+            del data['Unknown']  # we don't need this either
+            data['battery'] = packet.battery
+            packets.append(data)
+            packets_num = len(packets)
+            if packets_num >= size:
+                avgdata = {}
+                for key in data:
+                    if key == 'battery':
+                        # return the battery level for the last packet
+                        avgdata[key] = packets[-1][key]
+                    else:
+                        # calculate the average of sensors qualities and values
+                        # for all the packets
+                        qsum, vsum = 0, 0
+                        for p in packets:
+                            sensor_data = p[key]
+                            qsum += sensor_data['quality']
+                            vsum += sensor_data['value']
+                        avgdata[key] = dict(quality=qsum/packets_num,
+                                            value=vsum/packets_num)
+                print json.dumps(avgdata)
                 sys.stdout.flush()
-                xs.clear()
-                ys.clear()
+                packets.clear()
     except KeyboardInterrupt:
         headset.close()
     finally:
