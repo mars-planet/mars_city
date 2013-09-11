df4e21f373e1cae7409388e34dfdb5d077e81fcf
Ezio Melotti <ezio.melotti@gmail.com>
2013-09-11 21:49:31 +0300

    Add an HTML5 GUI and a minimal webserver to monitor the signal quality of the neuro headset.
    Former-commit-id: 96d23869086165d995c29729a70f83708446b700

---
+++ servers/neurohs/webserver.py
@@ -0,0 +1,33 @@
+from subprocess import Popen, PIPE
+
+def simple_app(environ, start_response):
+    path = environ['PATH_INFO']
+    if path == '/':
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        with open('eegpoints.html') as f:
+            return f.read()
+    elif path == '/eegdata':
+        start_response('200 OK', [('Content-Type', 'text/json')])
+        return headset.stdout.readline().strip()
+    elif path == '/eegpoints.gif':
+        # this is not currently used
+        start_response('200 OK', [('Content-Type', 'image/gif')])
+        with open('eegpoints.gif') as f:
+            return f.read()
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    print "Setting up headset (this might take a few seconds)...",
+    headset = Popen(['python', 'neurohs.py', '200'],
+                    stdout=PIPE, stderr=PIPE)
+    line = None
+    while line != '-----':
+        line = headset.stdout.readline().strip()
+
+    print "[OK]"
+
+    httpd = make_server('', 8080, simple_app)
+    print "Serving on http://localhost:8080/"
+
+    httpd.serve_forever()
