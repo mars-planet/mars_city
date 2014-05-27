daf2e0005d6fe75e3d63f8f65ef8022704b95eeb
Mathew Kallada <mt455727@dal.ca>
2014-05-27 14:16:30 -0300

    vision module: Added Compute Stereo Distance Function
    Former-commit-id: 349bb46e086ae6b5181b3343c316eec973e6580e

--- servers/vision/camera/depth.py
+++ servers/vision/camera/depth.py
@@ -2,10 +2,13 @@ import cv2
 import numpy as np
 from skimage import measure
 
+
 class DepthTrackerManager(object):
 
     def __init__(self):
         self._disparity_map = None
+        self._left_image = None
+        self._right_image = None
 
     @property
     def disparity_map(self):
@@ -14,6 +17,25 @@ class DepthTrackerManager(object):
             cv2.COLOR_GRAY2RGB)
         return rgb_disparity_frame
 
+    def compute_stereo_distance(self, metric="euclidean"):
+        """Compute the distance between the left and right image frames.
+        Possible values for metric keyword are: euclidean and manhattan.
+        """
+
+        if self._disparity_map is None:
+            raise ValueError("Must compute the disparity map first!")
+
+        difference_image = self._left_image - self._right_image
+
+        # Calculate distance between the two images
+        if metric == "euclidean":
+            distance = np.linalg.norm(difference_image)
+        elif metric == "manhattan":
+            distance = sum(abs(difference_image))
+        else:
+            raise ValueError("Supplied distance metric is not supported!")
+
+        return distance
 
     def compute_histogram(self, bins=10):
         """Returns the histogram of the disparity map. This gives us a general
@@ -24,7 +46,9 @@ class DepthTrackerManager(object):
 
         # Compute the histogram
         histogram = np.histogram2d(
-            x=self._disparity_map[:,1], y=self._disparity_map[:,0], bins=bins)
+            x=self._disparity_map[:, 1], y=self._disparity_map[:, 0],
+            bins=bins)
+
         return histogram
 
     def objects_in_proximity(self, min_size):
@@ -36,12 +60,13 @@ class DepthTrackerManager(object):
             raise ValueError("Must compute the disparity map first!")
 
         # Find the contours on the disparity map to find nearby objects
-        contours = measure.find_contours(self._disparity_map, level=0.1,
-            fully_connected='high', positive_orientation='high')
+        contours = measure.find_contours(self._disparity_map,
+                                         level=0.1, fully_connected='high',
+                                         positive_orientation='high')
 
         # Filter out the small objects, keeping only the close ones
         countours = [c for c in contours if len(c) > min_size]
-        
+
         return contours
 
     def compute_disparity(self, left_image, right_image,
@@ -53,10 +78,14 @@ class DepthTrackerManager(object):
             ndisparities=ndisparities,
             SADWindowSize=SADWindowSize)
 
+        # Convert the given images to grayscale
         gray_left = cv2.cvtColor(left_image,
                                  cv2.COLOR_BGR2GRAY)
+        self._left_image = gray_left
+
         gray_right = cv2.cvtColor(right_image,
                                   cv2.COLOR_BGR2GRAY)
+        self._right_image = gray_right
 
         # Compute stereo disparity image
         disparity_map = (
