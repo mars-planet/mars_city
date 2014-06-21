9f69aee9d943c2d5b808586fc923291aa2571b04
Alexander Demets <alexander.demets@gmail.com>
2014-06-21 12:21:32 -0700

    Added complete LibOVR Phyhon3 vanilla wrapper, added custom wrapper class for easier FusionSensor access, added preliminary documentation
    Former-commit-id: 1c1a5628256a8e01b426e408e1ffacff43fb5005

---
+++ servers/erasvr/pyOVR/RiftDevice.py
@@ -0,0 +1,18 @@
+from pyOVR import *
+####
+# Get orientation returns the Rotation Quaternion, which can then be set for the object 
+
+class RiftDevice(object):
+    def __init__(self):
+        ovr_Initialize()
+        self.hmd = ovrHmd_Create(0)
+        self.hmdDesc = ovrHmdDesc()
+        ovrHmd_GetDesc(self.hmd, byref(self.hmdDesc))
+        print(self.hmdDesc.ProductName)
+        ovrHmd_StartSensor( self.hmd, ovrSensorCap_Orientation | ovrSensorCap_YawCorrection, 0)
+    def __exit__(self, type, value, traceback):
+        ovrHmd_Destroy()
+    def getOrientation(self):
+        self.ss = ovrHmd_GetSensorState(self.hmd, ovr_GetTimeInSeconds())
+        pose = self.ss.Predicted.Pose
+        return pose.Orientation.w, pose.Orientation.x, pose.Orientation.y, pose.Orientation.z
---
+++ servers/erasvr/pyOVR/TestRiftDevice.py
@@ -0,0 +1,8 @@
+import time
+from RiftDevice import *
+
+rift = RiftDevice()
+
+while(True):
+    print(rift.getOrientation())
+    time.sleep(0.016) # sleep for a 60 fps refreshcycle
