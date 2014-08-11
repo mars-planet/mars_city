98c8fee0fa4ec0a010873c471ba86046ed8f67e0
Mathew Kallada <mt455727@dal.ca>
2014-08-11 15:44:00 -0300

    Improvements to Planning Structure + Enviroment Bug Fix
    Former-commit-id: c3d1463188389989d268e9225cca5310f12d7b3e

--- servers/pso/src/plan.py
+++ servers/pso/src/plan.py
@@ -15,7 +15,7 @@ def build_tree(log):
     return tree
 
 class Plan(object):
-    """A structured plan generated through a EUROPA temporal network log"""
+    """A structured plan generated from a EUROPA temporal network graph"""
 
     def __init__(self, europa_log):
         self.store = {}
@@ -23,25 +23,27 @@ class Plan(object):
         self.actions = {} # Objects that require actions to be made
         self.log = europa_log # The raw EUROPA log
 
-        # Get the objects currently in the planning environment
+        # Get all objects currently in the planning environment
         europa_log = europa_log.replace("*","")
-        environment_objects = re.findall(r'Variables(.*?)End Variables',europa_log, re.DOTALL)
+        environment_objects = re.findall(r'Variables(.*?)End Variables',
+            europa_log, re.DOTALL)
         for obj in environment_objects:
             # Build dictionary representation for ease of access
-            match = re.search(r' (.*?).name=',obj)
+            match = re.search(r'(.*?).name=',obj)
             if match:
                 object_name = match.group(1).strip()
 
-                # Clean unnessesary fluff from attributes        
-                attributes = re.findall(r'%s.(.*?)\n'%(object_name),europa_log, re.DOTALL)
+                # Clean unnessesary fluff from object attributes        
+                attributes = re.findall(r'%s.(.*?)\n'%(object_name),
+                    europa_log, re.DOTALL)
                 attributes = filter(lambda a: "=" in a, attributes)
                 attirbutes = map(lambda a: a.strip(),attributes)
 
-                objects[object_name] = {}
+                self.objects[object_name] = {}
                 for att in attributes:
                     var = att[:att.find("=")]
                     val = att[att.find("=")+1:]
-                    objects[object_name][var]=val
+                    self.objects[object_name][var]=val
 
 
         # Extract the Planned Actions to take place along with time constrainsts
@@ -64,8 +66,7 @@ class Plan(object):
                         event["upper"]=last_lower
                         last_lower = line
                         # Save and record past event
-                        if event_info:
-                            event_info = event_info[1:] # 1st action is faulty
+                        if "".join(event_info):
                             event["events"] = event_info
                             self.actions[actor_name].append(event)
                         event_info = []
