d06da47902caaa687e0c969e0cb4ea183db476c3
mtambos <devnull@localhost>
2014-12-05 15:59:11 +0100

    Change simulation to provide more realistic data.
    Former-commit-id: 808e047471b74d917ab2c328881b7941c48e5997

--- servers/health_monitor/src/AoudaDaemon/aoudadaemon/aouda.py
+++ servers/health_monitor/src/AoudaDaemon/aoudadaemon/aouda.py
@@ -5,11 +5,70 @@ from __future__ import division, print_function
 
 from collections import deque
 from collections import namedtuple
-from datetime import datetime
+from datetime import datetime, timedelta
 import gc
 
 import numpy as np
 import pandas as pd
+
+
+def _minmax_scale(data, min_x=-1, max_x=1):
+    minimum = np.min(data)
+    maximum = np.max(data)
+    data_std = (data - minimum) / (maximum - minimum)
+    return data_std * (max_x - min_x) + min_x
+
+
+def _load_data():
+    """
+    Loads hr and acc data into a pandas.DataFrame from a dataset file.
+    """
+    import Oger as og
+    import csv
+    now = datetime.now()
+    now -= timedelta(microseconds=now.microsecond)
+    periods = 500
+    o2 = og.datasets.mackey_glass(sample_len=periods,
+                                  n_samples=1,
+                                  seed=1)[0][0].flatten()
+    o2 = _minmax_scale(o2, 96, 99)
+    timestamp = pd.date_range(now, periods=periods, freq='500L')
+    temperature = og.datasets.mackey_glass(sample_len=periods,
+                                           n_samples=1,
+                                           seed=3)[0][0].flatten()
+    temperature = _minmax_scale(temperature, 36, 37.5)
+    air_flow = og.datasets.mackey_glass(sample_len=periods,
+                                        n_samples=1,
+                                        seed=5)[0][0].flatten()
+    air_flow = _minmax_scale(air_flow, 0, 100)
+    heart_rate = og.datasets.mackey_glass(sample_len=periods,
+                                          n_samples=1,
+                                          seed=7)[0][0].flatten()
+    heart_rate = _minmax_scale(heart_rate, 60, 70)
+    data = zip(*[o2, temperature, air_flow, heart_rate])
+    data = pd.DataFrame(data, columns=['o2', 'temperature',
+                                       'air_flow', 'heart_rate'],
+                        index=timestamp)
+    data = data.resample('250L', fill_method='pad')
+    last_row = data.xs(data.index[-1])
+    last_row.name = data.index[-1] + timedelta(seconds=0.25)
+    data = data.append(last_row)
+    acc_magn = og.datasets.mackey_glass(sample_len=2 * periods,
+                                        n_samples=1,
+                                        seed=11)[0][0].flatten()
+    acc_magn = _minmax_scale(acc_magn, 0, 5)
+    data['acc_magn'] = acc_magn
+    data = data.resample('10L', fill_method='pad')
+    with open('ecg_v1.csv', 'rb') as f:
+        reader = csv.reader(f)
+        reader.next()
+        ecg_v1 = [float(r[0]) for r in reader]
+    last_rows = data.iloc[-24:]
+    last_rows['index'] = last_rows.index + timedelta(seconds=0.01)
+    last_rows.set_index('index', inplace=True)
+    data = data.append(last_rows)
+    data['ecg_v1'] = ecg_v1
+    return data
 
 
 class Aouda(object):
@@ -28,7 +87,7 @@ class Aouda(object):
         if simulate:
             self.log_function('Loading Data')
             self.shift_data = shift_data
-            self.data = self._load_data()
+            self.data = _load_data()
         else:
             import ehealth as eh
             self.ehealth = eh.eHealthClass()
@@ -36,26 +95,6 @@ class Aouda(object):
             self.ehealth.initPulsioximeter(1)
         self.log_function('Finished constructing Aouda')
         self.ecg_v1_buffer = deque(maxlen=10000)
-
-    def _load_data(self):
-        """
-        Loads hr and acc data into a pandas.DataFrame from a dataset file.
-        """
-        import Oger as og
-        periods = 1000
-        columns = ['ecg_v1', 'o2', 'temperature', 'air_flow',
-                   'heart_rate', 'acc_magn']
-        data = og.datasets.mackey_glass(sample_len=periods,
-                                        n_samples=len(columns),
-                                        seed=50)
-        data = np.asanyarray(data).reshape((len(columns), periods))
-        data = data + np.abs(data.min())
-        data = data.transpose()
-        return pd.DataFrame(data=data,
-                            index=pd.date_range(datetime.now(),
-                                                periods=periods,
-                                                freq='500L'),
-                            columns=columns)
 
     def _shift_data(self, from_datetime):
         if self.shift_data and self.data.index[-1] <= from_datetime:
