2d31b6da4f1fb9b16ae690edd1c670a7eb44c9b6
Franco Carbognani <franco.carbognani@marssociety.it>
2015-04-25 23:26:20 +0200

    initial porting to DK2 restricted to DK1 capability reproduction, using python-ovrsdk, https://github.com/jherico/python-ovrsdk instead of pyOVR.py generated by ctypegen
    Former-commit-id: 356658fd030317db6a36db6699424f29b6154654

--- servers/erasvr/pyOVR/__init__.py
+++ servers/erasvr/pyOVR/__init__.py
@@ -1,49 +1,39 @@
-
-from ctypes import byref
-from .pyOVR import ovr_Initialize, ovrHmd_Create, ovrHmdDesc, ovrHmd_GetDesc, ovrHmd_StartSensor, ovrSensorCap_Orientation, ovrHmd_GetSensorState, ovrSensorCap_YawCorrection, ovr_GetTimeInSeconds, ovrHmd_Destroy, ovr_Shutdown, ovrFovPort, ovrEyeRenderDesc, ovrHmd_GetRenderDesc, ovrMatrix4f, ovrMatrix4f_Projection
+from ctypes import *
+from oculusvr import *
 
 class RiftDevice(object):
     def __init__(self):
         # init oculus sdk
-        ovr_Initialize()
-        self.hmd = ovrHmd_Create(0)
-        self.hmdDesc = ovrHmdDesc()
-        ovrHmd_GetDesc(self.hmd, byref(self.hmdDesc))
-        ovrHmd_StartSensor( self.hmd, ovrSensorCap_Orientation | ovrSensorCap_YawCorrection, 0)
-        
-        # init ovrFovPort struct for left/right eye
-        self.eyeFov = [ovrFovPort(), ovrFovPort()]
-        self.eyeFov[0] = self.hmdDesc.DefaultEyeFov[0]
-        self.eyeFov[1] = self.hmdDesc.DefaultEyeFov[1]
-        
-        # init eyeRenderDesc struct for left/right eye
-        self.eyeRenderDesc = [ovrEyeRenderDesc(), ovrEyeRenderDesc()]
-        self.eyeRenderDesc[0] = ovrHmd_GetRenderDesc(self.hmd, pyOVR.ovrEye_Left, self.eyeFov[0])
-        self.eyeRenderDesc[1] = ovrHmd_GetRenderDesc(self.hmd, pyOVR.ovrEye_Right, self.eyeFov[1])
-        
-        # setup projection matrices
-        self.proj = [ovrMatrix4f(), ovrMatrix4f()]
-        self.proj[0] = ovrMatrix4f_Projection(self.eyeRenderDesc[0].Fov, 0.01, 10000.0, True)
-        self.proj[1] = ovrMatrix4f_Projection(self.eyeRenderDesc[1].Fov, 0.01, 10000.0, True)
+        print('Rift Init started')
+        Hmd.initialize()
+        self.hmd = Hmd()
+        self.hmdDesc = cast(self.hmd.hmd,POINTER(ovrHmdDesc)).contents
+        print(self.hmdDesc.ProductName)
+        self.frame = 0
+        self.hmd.configure_tracking() 
+
+        self.eyeOffsets = [ ovrVector3f(), ovrVector3f() ]
+        for eye in range(0, 2):
+            self.eyeOffsets[eye] = 0.0,0.0,0.0
+
+        print('Rift Initialized')
+
         
     def __del__(self):
-        ovrHmd_Destroy(self.hmd)
-        ovr_Shutdown()
+        self.hmd.destroy()
+        self.hmd = None
+        Hmd.shutdown()
         print("LibOVR shutdown complete.")
     
-    def get_projection_matrix(self, index):
-        # create bge compatible proj matrix
-        table = [ [ 0 for i in range(4) ] for j in range(4) ]
-        for i in range(4):
-            for j in range(4):
-                table[i][j]= self.proj[index].M[i][j]
-        print(table)
-        return table
-
     def get_orientation(self):
-        self.ss = ovrHmd_GetSensorState(self.hmd, ovr_GetTimeInSeconds())
-        pose = self.ss.Predicted.Pose
-        return pose.Orientation.w, pose.Orientation.x, pose.Orientation.y, pose.Orientation.z
+        self.frame += 1
+        poses = self.hmd.get_eye_poses(self.frame, self.eyeOffsets)
+        #self.hmd.begin_frame(self.frame)
+        #for i in range(0, 2):
+        #    eye = self.hmdDesc.EyeRenderOrder[i]
+        eye = self.hmdDesc.EyeRenderOrder[0]
+        rot = poses[0].Orientation
+        return rot.w, rot.x, rot.y, rot.z
 
     def say_hello(self):
         print("Rift says hi!")
