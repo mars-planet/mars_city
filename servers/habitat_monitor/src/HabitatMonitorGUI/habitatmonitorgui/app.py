cb416f2c57fa852f7f3203c25282e861b5f1f541
Ambar Mehrotra <mehrotraambar@gmail.com>
2015-05-30 15:58:02 +0530

    Added code for adding new server -- Work in Progress
    Former-commit-id: e3759de78d1d94ee87b01464e7939c7a1160d784

---
+++ servers/habitat_monitor/src/HabitatMonitorGUI/habitatmonitorgui/app.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from habitat import Ui_MainWindow
+
+from PyTango import DeviceProxy
+
+
+class HabitatMonitor(QtGui.QMainWindow):
+
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.addDeviceAction.triggered.connect(self.add_device)
+
+    def add_device(self):
+        print "Add new device"
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 
+            'Enter Device Address:')
+        text = str(text)
+        
+        if ok:
+            try:
+                self.proxy = DeviceProxy(text)
+                print self.proxy.ping()
+                msgBox = QtGui.QMessageBox()
+                msgBox.setText('Device added successfully')
+                msgBox.addButton(QtGui.QPushButton('Ok'), QtGui.QMessageBox.YesRole)
+                ret = msgBox.exec_()
+                dev_attrs = self.proxy.get_attribute_list()
+            except:
+                print "Exceptions"
+                QtGui.QErrorMessage(self).showMessage("Incorrect Device Adderess")
+                # QtGui.QMessageBox.warning(self, "Error",
+                #             "Incorrect Device Adderess")
+
+
+        else:
+            QtGui.QMessageBox.critical(self, "Warning",
+                            "Device not added")
+        
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = HabitatMonitor()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ servers/habitat_monitor/src/HabitatMonitorGUI/habitatmonitorgui/habitat.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'habitat.ui'
+#
+# Created: Sat May 30 10:04:46 2015
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName(_fromUtf8("menuFile"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.addDeviceAction = QtGui.QAction(MainWindow)
+        self.addDeviceAction.setObjectName(_fromUtf8("add_device"))
+        self.menuFile.addAction(self.addDeviceAction)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        self.addDeviceAction.setText(_translate("MainWindow", "Add Device", None))
+
