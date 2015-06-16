43c7cc6d532181ee160b8678e82275830054df53
Siddhant Shrivastava <sidhu94@gmail.com>
2015-06-16 18:25:53 +0530

    Improved setup-device functionality. Supports registering and unregistering of functions. Python Version agnostic with intuitive CLI
    Former-commit-id: 0162f6a27fa33a92e9d13ca77b1e94f328c45ad9

--- servers/telerobotics/utility/setup-device.py
+++ servers/telerobotics/utility/setup-device.py
@@ -1,48 +1,81 @@
 #!/usr/bin/env python2.7
 
+from __future__ import print_function
 import PyTango
 import sys
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 # A function which prints the received message and returns the input
 def prompt(message):
 
-    # display the message prompt to standard output
-
-    sys.stdout.write(message)
-
     # Using Python version agnostic functions to take input efficiently
-    user_input = sys.stdin.readline()
+    user_input = input(message)
     user_input = user_input.replace("\n", "")
     return user_input
 
-# check for validity of arguments
-if len(sys.argv) != 1:
-    sys.stdout.write(
-        "This interactive script takes no arguments\nFormat: python setup-device.py\n")
-    sys.exit(255)
+def register(tango_database, device_info):
 
-sys.stdout.write(
-    "This script is an easy way to setup a Device in the Tango" \
-    " database from the command line\n\n")
+    try:
+        # Add the device to the database
+        tango_database.add_device(device_info)
+        print("\nDevice Successfully added and registered!\n")
+    except PyTango.DevFailed, err:
+        PyTango.Except.print_exception(err)
+        sys.exit()
 
-# 'device_info' is a structure containing available information for a device with
-# the following members:
-#    - name : (str) name
-#    - _class : (str) device class
-#    - server : (str) server
+def unregister(tango_database, device_info):
 
-device_info = PyTango.DbDevInfo()
-device_info.server = prompt(
-    "Server Name (eg. telerobotics/instance_name) : ")
-device_info._class = prompt("Class Name (eg. PyTelerobotics): ")
-device_info.name = prompt(
-    "Canonical Device Name (eg. C3/GUI/telerobotics) : ")
+    try:
+        # Remove the device from the database
+        tango_database.delete_server(device_info.server)
+        tango_database.delete_device(device_info.name)
+        print("\nDevice Successfully unregistered and removed!\n")
 
-# Get an Instance of the Tango Database (device server)
-tango_database = PyTango.Database()
+    except PyTango.DevFailed, err:
+        PyTango.Except.print_exception(err)
+        sys.exit()
 
-# Add the device to the database
-tango_database.add_device(device_info)
+def init():
 
-# Need to add PyTango exceptions here
-sys.stdout.write("Device Successfully added!\n")
+    # check for validity of arguments
+    if len(sys.argv) != 2:
+        print(
+            "This interactive script takes one arguments\n"
+            "Register format: python setup-device.py <add,setup,register>\n"
+            "Unregister format: python setup-device.py <del,delete,unregister>\n")
+        sys.exit(255)
+
+    print(
+        "This script is an easy way to setup/delete a Device in the Tango"
+        " database from the command line\nEnter the Device details below:\n")
+        # 'device_info' is a structure containing available information for
+    # a device with the following members:
+    #    - name : (str) name
+    #    - _class : (str) device class
+    #    - server : (str) server
+
+    device_info = PyTango.DbDevInfo()
+    device_info.server = prompt(
+        "Server Name (eg. telerobotics/instance_name) : ")
+    device_info._class = prompt("Class Name (eg. PyTelerobotics): ")
+    device_info.name = prompt(
+        "Canonical Device Name (eg. C3/GUI/telerobotics) : ")
+
+    # Get an Instance of the Tango Database (device server)
+    tango_database = PyTango.Database()
+
+    if(sys.argv[1] in ('add', 'setup' , 'register')):
+        register(tango_database, device_info)
+
+    elif(sys.argv[1] in ('del' , 'delete' , 'unregister')):
+        unregister(tango_database, device_info)
+
+    else:
+        print("Argument not understood. Try again.\n")
+
+if __name__ == '__main__' :
+    init()
