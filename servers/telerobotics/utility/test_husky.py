475b3b3db1264ea7f2f3d87e2a78abca16afa4aa
Siddhant Shrivastava <sidhu94@gmail.com>
2015-06-20 20:39:45 +0530

    Added Exception Handling and automation for Husky tests
    Former-commit-id: b044f5cae70ab09b98124e7eaa3d41628b4ac55d

--- servers/telerobotics/utility/test_husky.py
+++ servers/telerobotics/utility/test_husky.py
@@ -1,46 +1,56 @@
-"""A test script to run a Husky UGV for 3 seconds"""
+"""A test script to run a Husky UGV for MOVE_TIME * SLEEP_RATE seconds"""
 
 import roslib
 import rospy
 from geometry_msgs.msg import Twist
+import subprocess
 
 LINEAR_VELOCITY = 3  # Configuration Variable for speed along the x direction  - 3 m/s
-ANGULAR_VELOCITY = 2 # Configuration Variable for angular velocity for z-axis
-# first thing, init a node!
-rospy.init_node('commander')
+ANGULAR_VELOCITY = 2  # Configuration Variable for angular velocity for z-axis
+MOVE_TIME = 40  # Husky runs for (MOVE_TIME / 10) seconds
+SLEEP_RATE = 0.1
+HUSKY_TOPIC = '/husky_velocity_controller/cmd_vel'
+TIMEOUT = 5
 
 def command():
 
-	# publish to the 'cmd_vel' ROS topic
-	p = rospy.Publisher('husky_velocity_controller/cmd_vel', Twist, queue_size = 100)
+    topics = rospy.get_published_topics()
 
-	# create a twist message, and set up the linear and angular
-	# velocity arguments
-	twist = Twist()
-	twist.linear.x = LINEAR_VELOCITY
-	twist.linear.y = 0
-	twist.linear.z = 0
-	twist.angular.x = 0
-	twist.angular.y = 0;
-	twist.angular.z = ANGULAR_VELOCITY;
+    if HUSKY_TOPIC not in topics:
+        # launch the relevant ros topics
+        subprocess.Popen(["roslaunch", "husky_control", "control.launch"])
+        # publish to the 'cmd_vel' ROS topic
+        p = rospy.Publisher(HUSKY_TOPIC, Twist, queue_size=100)
+        rospy.sleep(TIMEOUT)
 
-	# announce move, and publish the message
-	rospy.loginfo("In Motion!")
+    # create a twist message, and set up the linear and angular
+    # velocity arguments
+    twist = Twist()
+    twist.linear.x = LINEAR_VELOCITY
+    twist.angular.z = ANGULAR_VELOCITY
 
-	#publish rate
-	for i in range(90):
-	    p.publish(twist)
-	    rospy.sleep(0.1) # 30*0.1 = 3.0 seconds
+    # announce move, and publish the message
+    rospy.loginfo("In Motion!")
 
-	# create a new message
-	twist = Twist()
+    # Publish Rate - Husky moves for MOVE_TIME * SLEEP_RATE seconds
+    for i in range(MOVE_TIME):
+        p.publish(twist)
+        rospy.sleep(SLEEP_RATE)
 
-	# note: everything defaults to 0 in twist, if we don't fill it in, we stop!
-	rospy.loginfo("Stopping now!")
-	p.publish(twist)
+    # create a new message
+    twist = Twist()
+    rospy.loginfo("Stopping now!")
+    p.publish(twist)
 
 if __name__ == '__main__':
-	try:
-	    command()
-	except rospy.ROSInterruptException:
-	    pass
+    try:
+        # Create and Initialize a ROS node!
+        rospy.init_node('commander')
+    except rospy.ROSInitException as init_exception:
+        print("roscore is not initiated yet. Message: %s") % init_exception
+        roscore = subprocess.Popen(['roscore'])
+
+    try:
+        command()
+    except rospy.ROSInterruptException as interrupt:
+        print("ROS process was interrupted")
