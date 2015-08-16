a98364ed0f1c5924c271e9212eb2a8e8990517b4
Shridhar Mishra <shridharmishra4@gmail.com>
2015-08-17 00:54:00 +0530

    Cleaning up Europa-pso part of the project to a working state.
    Former-commit-id: c12d8395282065a549774ebb536fe93aa15aa562

--- servers/planning/src/simple_example/main.py
+++ servers/planning/src/simple_example/main.py
@@ -12,7 +12,7 @@ WHITE    = ( 255, 255, 255)
 RED      = ( 255,   0,   0)
 
 # Connect to planner device via PyTango
-planner= DeviceProxy("c3/waldo/pso")
+planner= DeviceProxy("C3/pso/1")
 
 def get_actions():
     return planner.get_actions()
@@ -45,16 +45,16 @@ class Object(pygame.sprite.Sprite):
     def __init__(self, image, x, y):
 
         # Call the parent class (Sprite) constructor
-        pygame.sprite.Sprite.__init__(self) 
-  
+        pygame.sprite.Sprite.__init__(self)
+
         self.image, self.rect = load_image(image, -1)
         self.rect.x = x
         self.rect.y = y
 
     def reset_pos(self):
         self.rect.y = random.randrange(-300, -20)
-        self.rect.x = random.randrange(0, screen_width)        
- 
+        self.rect.x = random.randrange(0, screen_width)
+
     def update(self):
         if self.moving:
 
@@ -78,65 +78,65 @@ class Object(pygame.sprite.Sprite):
 class Rover(Object):
 
     def __init__(self, x ,y):
-        Object.__init__(self, "rover.png", x, y) 
+        Object.__init__(self, "rover.png", x, y)
 
 class Rock(Object):
 
     def __init__(self, name, x ,y):
         self.name = name
-        Object.__init__(self, "rock.png", x, y) 
- 
+        Object.__init__(self, "rock.png", x, y)
+
 
 # Initialize Pygame
 pygame.init()
-  
+
 # Set the height and width of the screen
 screen_width = 700
 screen_height = 400
 screen = pygame.display.set_mode([screen_width, screen_height])
-  
+
 # This is a list of 'sprites.' Each rover in the program is
 # added to this list. The list is managed by a class called 'Group.'
 rock_list = pygame.sprite.Group()
-  
+
 # This is a list of every sprite. All rocks and the rover as well.
 all_sprites_list = pygame.sprite.Group()
 
-  
+
 # Create a rover on screen
 rover = Rover(0, 0)
 all_sprites_list.add(rover)
 
-# Create a new rock on screen      
+# Create a new rock on screen
 rock1 = Rock("rock1",90*4, 90*4)
 rock_list.add(rock1)
 all_sprites_list.add(rock1)
-  
-# Create a new rock on screen      
+
+# Create a new rock on screen
 rock2 = Rock("rock2",10*4, 60*4)
 rock_list.add(rock2)
 all_sprites_list.add(rock2)
 
-# Create a new rock on screen      
+# Create a new rock on screen
 rock3 = Rock("rock3",40*4, 80*4)
 rock_list.add(rock3)
 all_sprites_list.add(rock3)
 
-# Create a new rock on screen      
+# Create a new rock on screen
 rock4 = Rock("rock4",30*4, 90*4)
 rock_list.add(rock4)
 all_sprites_list.add(rock4)
-      
 
-  
+
+
 #Loop until the user clicks the close button.
 done = False
-  
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-  
+
 score = 0
-  
+
 # -------- Main Program Loop -----------
 while not done:
 
@@ -150,6 +150,7 @@ while not done:
 
             if ".Go" in event:
                 destination=re.search("dest={(.*)[/(]", event).group(1)
+                print destination 
                 for sprite in list(rock_list):
                     if sprite.name==destination:
                         rover.move_to(sprite)
@@ -159,23 +160,23 @@ while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
-  
+
     # Clear the screen
     screen.fill(WHITE)
-     
+
     # Calls update() method on every sprite in the list
     all_sprites_list.update()
-      
+
     # See if the rover has collided with anything.
-    blocks_hit_list = pygame.sprite.spritecollide(rover, rock_list, False)  
+    blocks_hit_list = pygame.sprite.spritecollide(rover, rock_list, False)
 
     # Draw all the spites
     all_sprites_list.draw(screen)
-      
+
     # Limit to 20 frames per second
     clock.tick(20)
-  
+
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
-  
+
 pygame.quit()
