fa41dc54d8e745abefef64e064c09bd441a09cfd
Shridhar Mishra <shridharmishra4@gmail.com>
2016-08-09 15:46:33 +0530

    File handling added

--- servers/unity/linux/plotlines.cs
+++ servers/unity/linux/plotlines.cs
@@ -1,5 +1,39 @@
- using UnityEngine;
+using UnityEngine;
 using System.Collections;
+
+private bool Load(string fileName)
+ {
+     // Handle any problems that might arise when reading the text
+     try
+     {
+         string line;
+         StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+      
+         using (theReader)
+         {
+             do
+             {
+                 line = theReader.ReadLine();
+                     
+                 if (line != null)
+                 {
+                     string[] entries = line.Split(',');
+                     if (entries.Length > 0)
+                         print(entries);
+                 }
+             }
+             while (line != null);
+             theReader.Close();
+             return true;
+             }
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine("{0}\n", e.Message);
+             return false;
+         }
+     }
+ }
 
 public class ExampleClass : MonoBehaviour {
     public Material mat;
