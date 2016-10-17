cc86bf31ee17c65b0e460125f68e0dcdc0355b87
abhijith0505 <abhijith4m0505@gmail.com>
2016-10-17 19:49:48 +0530

    Revert "PEP8 Compliance on voicergn"
    This reverts commit fda856fbba9846888e0ed863f9acb958d51862d1.

--- servers/voicergn/voice_recognition/workingbackup/allnew.py
+++ servers/voicergn/voice_recognition/workingbackup/allnew.py
@@ -31,74 +31,74 @@ import audioop
 RATE = 16000
 CHUNK = 1024
 
-# run importacoustic.sh
+#run importacoustic.sh
 #hmdir = "~/voicelib/hub4wsj_sc_8kadapt"
 hmdir = "hub4wsj_sc_8kadapt"
 lmdir = "6491.lm"
 dictd = "a.dic"
 sysdir = os.getcwd
 
-
 def getScore(data):
-    rms = audioop.rms(data, 2)
-    score = rms / 3
-    return score
+	rms = audioop.rms(data, 2)
+	score = rms / 3
+	return score
 
 
 def fetchThreshold():
 
-    THRESHOLD_MULTIPLIER = 1.8
-    AUDIO_FILE = "passive.wav"
 
-    # number of seconds to allow to establish threshold
-    THRESHOLD_TIME = 1
+	THRESHOLD_MULTIPLIER = 1.8
+	AUDIO_FILE = "passive.wav"
 
-    # number of seconds to listen before forcing restart
-    LISTEN_TIME = 10
 
-    # prepare recording stream
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
+	# number of seconds to allow to establish threshold
+	THRESHOLD_TIME = 1
 
-    # stores the audio data
-    frames = []
+	# number of seconds to listen before forcing restart
+	LISTEN_TIME = 10
 
-    # stores the lastN score values
-    lastN = [i for i in range(20)]
+	# prepare recording stream
+	audio = pyaudio.PyAudio()
+	stream = audio.open(format=pyaudio.paInt16,
+						channels=1,
+						rate=RATE,
+						input=True,
+						frames_per_buffer=CHUNK)
 
-    # calculate the long run average, and thereby the proper threshold
-    for i in range(0, RATE / CHUNK * THRESHOLD_TIME):
+	# stores the audio data
+	frames = []
 
-        data = stream.read(CHUNK)
-        frames.append(data)
+	# stores the lastN score values
+	lastN = [i for i in range(20)]
 
-        # save this data point as a score
-        lastN.pop(0)
-        lastN.append(getScore(data))
-        average = sum(lastN) / len(lastN)
+	# calculate the long run average, and thereby the proper threshold
+	for i in range(0, RATE / CHUNK * THRESHOLD_TIME):
 
-    # this will be the benchmark to cause a disturbance over!
-    THRESHOLD = average * THRESHOLD_MULTIPLIER
+		data = stream.read(CHUNK)
+		frames.append(data)
 
-    return THRESHOLD
+		# save this data point as a score
+		lastN.pop(0)
+		lastN.append(getScore(data))
+		average = sum(lastN) / len(lastN)
 
+	# this will be the benchmark to cause a disturbance over!
+	THRESHOLD = average * THRESHOLD_MULTIPLIER
+
+	return THRESHOLD
 
 def decodepassive():
-    speechRec = Decoder(hmm=hmdir, lm=lmdir, dict=dictd)
-    with open(passivewav, 'rb') as passivewav:
-        speechRec.decode_raw(passivewav)
-    result = speechRec.get_hyp()
-    return(result[0])
-    # print "-----------------------"
-    # print result[0]
-    # print "-----------------------"
-
+	speechRec = Decoder(hmm = hmdir, lm = lmdir, dict = dictd)
+	with open(passivewav, 'rb') as passivewav:
+		speechRec.decode_raw(passivewav)
+        result = speechRec.get_hyp()
+        return(result[0])
+	#print "-----------------------"
+	#print result[0]
+	#print "-----------------------"
 
 def record(THRESHOLD=None):
+
 
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
@@ -107,8 +107,9 @@ def record(THRESHOLD=None):
 
     p = pyaudio.PyAudio()
     if THRESHOLD == None:
-        THRESHOLD = fetchThreshold()
-        print THRESHOLD
+		THRESHOLD = fetchThreshold()
+		print THRESHOLD
+
 
     stream = p.open(format=FORMAT,
                     channels=1,
@@ -116,22 +117,26 @@ def record(THRESHOLD=None):
                     input=True,
                     frames_per_buffer=CHUNK)
 
+
+
     print "* recording"
     frames = []
     lastN = [THRESHOLD * 1.2 for i in range(30)]
     for i in range(0, RATE / CHUNK * LISTEN_TIME):
-        data = stream.read(CHUNK)
-        frames.append(data)
-        score = getScore(data)
-        lastN.pop(0)
-        lastN.append(score)
-        average = sum(lastN) / float(len(lastN))
-        # print average,THRESHOLD * 0.8
-        if average < THRESHOLD * 0.8:
-            break
+		data = stream.read(CHUNK)
+		frames.append(data)
+		score = getScore(data)
+		lastN.pop(0)
+		lastN.append(score)
+		average = sum(lastN) / float(len(lastN))
+		#print average,THRESHOLD * 0.8
+		if average < THRESHOLD * 0.8:
+			break
+
+
 
     print "* done recording"
-    # stream.stop_stream()
+    #stream.stop_stream()
     stream.close()
     p.terminate()
 
@@ -144,26 +149,29 @@ def record(THRESHOLD=None):
     wf.writeframes(data)
     wf.close()
     sysdir = os.getcwd()
-    wavfile = sysdir + "/livewav.wav"
-    # decoded=decodepassive()
+    wavfile = sysdir+"/livewav.wav"
+    #decoded=decodepassive()
+
 
     speechRec = Decoder(hmm=hmdir, lm=lmdir, dict=dictd)
     with open(wavfile, 'rb') as wavFile:
         speechRec.decode_raw(wavFile)
         result = speechRec.get_hyp()
 
+
     return(result[0])
 
-    # DO SOME AMPLIFICATION
+    #DO SOME AMPLIFICATION
     #os.system("sox "+WAVE_OUTPUT_FILENAME+" temp.wav vol 15dB")
 
 
 def main():
-    a = record()
-    print a
-    print "done"
+	a=record()
+	print a
+	print "done"
 
-    return 0
+	return 0
 
 if __name__ == '__main__':
-    main()
+	main()
+
