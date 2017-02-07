953ced9bcf2b9d139dd94ee94a7566cb337078b1
Abhijith C <abhijith4m0505@gmail.com>
2017-02-07 19:51:55 +0530

    Descendant helper done

--- MOCC/src/configurations/Get-Descendant/src/DescendantHelper.py
+++ MOCC/src/configurations/Get-Descendant/src/DescendantHelper.py
@@ -3,5 +3,19 @@
 from __future__ import division, print_function
 import json
 
+META_CHARACTER_SEPARATOR = ":"
 
-def getDescendants(dev):
+def getDescendants(device_id, dev_names):
+	devices = []
+	for dev in dev_names:
+		if dev == device_id:
+			pass
+		else:
+			devices.append(dev)
+
+	descendants = []
+	for dev in devices:
+		if device_id in dev:
+			descendants.append(dev)
+
+	return	descendants
