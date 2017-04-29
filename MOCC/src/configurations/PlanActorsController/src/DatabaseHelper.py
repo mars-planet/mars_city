661f847c75fbb27335020885934052ab189c4db2
Abhijith C <abhijith4m0505@gmail.com>
2017-04-29 09:08:52 +0530

    Added PlanActorsController service - pep8 compliant

--- MOCC/src/configurations/PlanActorsController/src/DatabaseHelper.py
+++ MOCC/src/configurations/PlanActorsController/src/DatabaseHelper.py
@@ -1,52 +1,53 @@
 import sqlite3 as sq
 
-con = sq.connect('plan_actors')
+con = sq.connect('plan_actors', check_same_thread=False)
 
-## Create the initial database
-## 
-## Deletes old and creates new
+
 def createDatabase():
-	con.execute('DROP TABLE IF EXISTS plan_actors;')
-	con.execute('''CREATE TABLE plan_actors
-		(type TEXT NOT NULL,
-		address TEXT PRIMARY KEY NOT NULL,
-		avail_start DATE NOT NULL,
-		avail_end DATE NOT NULL,
-		capabilities TEXT NOT NULL);''')
-	print("Table created successfully");
-	
-## Add new entry of the plan_actors into the database
-##
-## data - dictionary containing necessary field in order
+    con.execute('DROP TABLE IF EXISTS plan_actors;')
+    con.execute("CREATE TABLE plan_actors\
+    (type TEXT NOT NULL,\
+    address TEXT PRIMARY KEY NOT NULL,\
+    avail_start DATE NOT NULL,\
+    avail_end DATE NOT NULL,\
+    capabilities TEXT NOT NULL);")
+    print("Table created successfully")
+
+
 def add(data):
-	type_t = data[0]
-	address = data[1]
-	avail_start = data[2]
-	avail_end = data[3]
-	capabilities = data[4]
-	
-	query = '''INSERT INTO plan_actors VALUES
-		(\'''' + type_t +'''\',
-		\'''' + address +'''\',
-		\'''' + avail_start +'''\',
-		\'''' + avail_end +'''\',
-		\'''' + capabilities +'''\');'''
-	con.execute(query)
-	
-	print("Inserted row successfully")
+    type_t = data[0]
+    address = data[1]
+    avail_start = data[2]
+    avail_end = data[3]
+    capabilities = data[4]
 
-## Retreives entry from the table where address of plan_actor = address
-##
-##
+    query = '''INSERT INTO plan_actors VALUES
+    (\'''' + type_t + '''\',
+    \'''' + address + '''\',
+    \'''' + avail_start + '''\',
+    \'''' + avail_end + '''\',
+    \'''' + capabilities + '''\');'''
+    try:
+        con.execute(query)
+        con.commit()
+    except sq.IntegrityError:
+        return -1
+
+    print("Inserted row successfully")
+    return 0
+
+
 def get(address):
-	return_data = []
-	
-	query = '''SELECT * FROM plan_actors WHERE
-		ADDRESS == \'''' + address + '''\';'''
-	cursor = con.execute(query)	
-	
-	return cursor
+    return_data = []
 
+    query = '''SELECT * FROM plan_actors WHERE
+    ADDRESS == \'''' + address + '''\';'''
+    cursor = con.execute(query)
 
-
-
+    for row in cursor:
+        return_data.append(row[0])
+        return_data.append(row[1])
+        return_data.append(row[2])
+        return_data.append(row[3])
+        return_data.append(row[4])
+    return return_data
