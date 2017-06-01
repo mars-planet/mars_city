57f5dad4da584074d00adbf1cfb9e32973ae6e94
aneesh-joshi <aneeshyjoshi@gmail.com>
2017-06-01 06:02:25 +0530

    removed sb approach

--- MOCC/src/configurations/test_aneesh/resource_manager_tester.py
+++
@@ -1,9 +0,0 @@
-import PyTango
-
-device = PyTango.DeviceProxy("test/resource_manager/1")
-
-print device.ping()," device pinged\n"
-
-resource_data = device.ask_resource("Battery")
-
-print "Resource data received is :\n",resource_data
