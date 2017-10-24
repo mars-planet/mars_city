d4edc0b79223002b6c444a68ba32ab39c6b07ee3
Arpit Gogia <arpitgogia@protonmail.com>
2017-10-24 13:13:56 +0530

    Added Swagger definitions for Health Monitor

--- MOCC/src/configurations/Comm-Layer/HealthMonitorDS.py
+++ MOCC/src/configurations/Comm-Layer/HealthMonitorDS.py
@@ -12,25 +12,54 @@ Swagger(app)
 
 @app.route('/health_monitor/functions/<string:fn_name>', methods=['GET'])
 def execute_function(fn_name):
-    # TODO Make function calls here
-    # HOW?
+    """Execute a function of the Health Monitor
+    ---
+    tags:
+      - health
+      - monitor
+      - execute
+    parameters:
+      - name: fn_name
+        in: path
+        type: string
+        required: true
+    responses:
+      200:
+        description: When the function is executed successfully.
+        type: string
+    """
     return jsonify({'status': 'SUCCESSFULLY executed ' + fn_name})
 
 
 @app.route('/health_monitor/functions', methods=['GET'])
 def list_functions():
-    '''
-    return all the functions that the Health Monitor has
-    '''
+    """Returns all the functions of the Health Monitor
+    ---
+    tags:
+      - healh
+      - monitor
+      - function
+    responses:
+      200:
+        description: Functions of the health monitor.
+        type: JSON
+    """
     functions = ['administer_insulin', 'evacuate', 'ressucitate']
     return jsonify({'functions': functions})
 
 
 @app.route('/health_monitor/attributes', methods=['GET'])
 def list_attributes():
-    '''
-    return all the attributes that the Health Monitor has
-    '''
+    """Return all the attributes belonging to the Health Monitor
+    ---
+    tags:
+      - health
+      - monitor
+    responses:
+      200:
+        description: Returns the attributes of the Health Monitor.
+        type: JSON
+    """
     attributes = ['health', 'heart_rate', 'oxygen_level']
     return jsonify({'attributes': attributes})
 
