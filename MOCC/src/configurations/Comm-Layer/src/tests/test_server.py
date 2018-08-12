d723483a7ae13d8873c861f9936e09f24b457fca
Mario Tambos <mtambos@gmail.com>
2018-08-12 12:23:32 +0200

    send data as json

--- MOCC/src/configurations/Comm-Layer/src/tests/test_server.py
+++ MOCC/src/configurations/Comm-Layer/src/tests/test_server.py
@@ -1,21 +1,20 @@
 import pytest
-import sys
 import uuid
-sys.path.insert(0, '..')
 
-from server import ServerBase, Command, Server
+from server import ServerBase, Command
 
-#  class Server(ServerBase):
-    #  """
-    #  Sample server.
-    #  """
-    #  @Command
-    #  def cmd1(self, arg1, arg2):
-        #  ret_val = {
-            #  'arg1': arg1,
-            #  'arg2': arg2,
-        #  }
-        #  return ret_val
+
+class Server(ServerBase):
+    """
+    Sample server.
+    """
+    @Command
+    def cmd1(self, arg1, arg2):
+        ret_val = {
+            'arg1': arg1,
+            'arg2': arg2,
+        }
+        return ret_val
 
 
 @pytest.fixture
@@ -32,8 +31,9 @@ def test_cmd1(client):
         'arg1': str(uuid.uuid4()),
         'arg2': str(uuid.uuid4()),
     }
-    rv = client.post('/cmd1', data=data)
+    rv = client.post('/cmd1', json=data)
     assert data == rv.json
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
